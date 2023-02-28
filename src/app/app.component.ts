ea8126f1e954a389c40d0469d19b92acb4862ccf
Ashutosh Maurya <dev26@bytescrum.com>
2023-02-28 16:33:20 +0530

    git users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { AppService } from './app.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'github-users';
+  usersList: any = [];
+  constructor(private user: AppService) {}
+
+  ngOnInit() {
+    this.user.getData().subscribe((data: any) => (this.usersList = [...data]));
+  }
 }
